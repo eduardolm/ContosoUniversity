d08856f3d1f0ca7209a5c305d9490f83fe045f89
Eduardo <lodi001@uol.com.br>
2020-06-10 12:46:03 -0300

    Finished building the app

---
+++ ContosoUniversity/Views/Departments/Details.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoUniversity.Views.Departments
+{
+    public class Details : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
