f000a73b7352617208abe46a213110a1f57189dd
Eduardo <lodi001@uol.com.br>
2020-06-09 22:37:38 -0300

    Changed the name of one column and added maximum length for both name and lastname.

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        
+        [StringLength(50)]
         public string LastName { get; set; }
-        public string FirstName { get; set; }
+        
+        [StringLength(50)]
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+        
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
         
         public ICollection<Enrollment> Enrollments { get; set; }
